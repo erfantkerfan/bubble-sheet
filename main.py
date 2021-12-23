b4b4fbe690d1763cd219a75b511d656d00bfd962
erfantkerfan <erfantkerfan@yahoo.com>
2021-12-23 22:28:15 +0330

    crank up version number

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def create_app():
 
 
 if __name__ == '__main__':
-    VERSION = '0.4.0'
+    VERSION = '1.2.0'
 
     parser = argparse.ArgumentParser(description='Process bubble sheets.')
     parser.add_argument('-v', '--version', action='version', version=f'{VERSION}')
